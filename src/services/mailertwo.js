b2ab062a8709639f687daa9fac6fc463e8d528e2
Ruth Iroabuchi <Roani@Austins-MacBook-Pro.local>
2023-06-05 20:54:52 +0100

    removed bcc from hubspot

--- src/services/mailertwo.js
+++ src/services/mailertwo.js
@@ -21,7 +21,7 @@ const sendhtmlEMAIL=async (email,subject,html)=>{try {
 let info = await transporter.sendMail({
     from: process.env.MAIL_FROM, // sender address
     to: email, // list of receivers
-    bcc: "devruthani@gmail.com",
+    // bcc: "devruthani@gmail.com",
     subject: subject, // Subject line
    
     html:html, // html body
